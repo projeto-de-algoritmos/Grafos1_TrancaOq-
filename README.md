574cc7a6fe190956e977d96f13cece60771f8966
Matheus Gabrie <matheuscoding@gmail.com>
2021-07-30 23:43:16 -0300

    Adicionando arquivo com a implementação de grafos e modificando o README.md
    co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Grafos1_HobbitRoot
-Projeto de Grafos 1 onde se cria uma arvore genealógica dos Hobbit citados em 'O senhor dos anéis'
+# Grafos2_HobbitRoot
+
+**Número da Lista**: X<br>
+**Conteúdo da Disciplina**: Grafos 1
+
+## Alunos
+
+|Matrícula | Aluno |
+| -- | -- |
+| 18/0106970  |  Matheus Gabriel Alves Rodrigues |
+| 18/0129287 |  Pedro Henrique Vieira Lima |
+
+## Sobre
+
+Projeto de Grafos 1 onde se cria uma arvore genealógica dos Hobbit citados em 'O senhor dos anéis',
+o projeto busca mostrar com base em um nome de um determinado Hobbit os seus parentes mais próximos
+como pais,filhos e cônjuges.
+
+## Screenshots
+
+Adicione 3 ou mais screenshots do projeto em funcionamento.
+
+## Instalação
+
+**Linguagem**: Python<br>
+**Framework**: (caso exista)<br>
+Descreva os pré-requisitos para rodar o seu projeto e os comandos necessários.
+
+## Uso
+
+Explique como usar seu projeto caso haja algum passo a passo após o comando de execução.
+
+## Outros
+
+Quaisquer outras informações sobre seu projeto podem ser descritas abaixo.
---
+++ graph.py
@@ -0,0 +1,62 @@
+class Vertex:
+    def __init__(self, name, gender, spouse, wiki, realm):
+        self.name = name
+        self.gender = gender
+        self.spouse = spouse
+        self.realm = realm
+        self.wiki = wiki
+        self.adjacent = {}
+
+    def add_neighbor(self, neighbor, kinship):
+        self.adjacent[neighbor] = kinship
+
+    def get_connections(self):
+        return self.adjacent.keys()
+
+    def get_name(self):
+        return self.name
+
+    def get_data(self):
+        return self.name, self.gender, self.spouse, self.wiki, self.realm
+
+    def get_kinship(self, neighbor):
+        return self.adjacent[neighbor]
+
+    def __str__(self):
+        return f"{self.get_name()} -> {[x.get_name() for x in self.adjacent]}"
+
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vert = 0
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, name, gender, spouse, wiki, realm):
+        self.num_vert += 1
+        new_vertex = Vertex(name,gender,spouse,wiki,realm)
+        self.vert_dict[name] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, name):
+        if name in self.vert_dict:
+            return self.vert_dict[name]
+        else:
+            return None
+
+    def add_edge(self, frm, to, kinship):
+        if frm not in self.vert_dict:
+            return None
+        if to not in self.vert_dict:
+            return None
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to],kinship)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm],kinship)
+
+    def get_vertices(self):
+        return list(self.vert_dict.keys())
+
+
+
+
+
