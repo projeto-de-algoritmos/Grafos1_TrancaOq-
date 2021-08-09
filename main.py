ce216ba391a3d459b7b2da2dded8b2ff7f6b9124
Matheus Gabrie <matheuscoding@gmail.com>
2021-08-09 11:36:53 -0300

    Adicionando front-end basico
    Co-authored-by: pedrok99 <pedrohlima20@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pandas import read_csv
 from graphutils import Graph, get_dependents, get_dependencies
 
-from flask import Flask, json, render_template
+from flask import Flask, json, render_template,request
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='front')
 
 df = read_csv('grade_software.csv').values.tolist()
 
@@ -17,9 +17,18 @@ for lista in df:
 grafo = Graph(edges = edges, directed = True)
 
 
-@app.route('/')
-def home():
-    return 'Olá mundo'
+@app.route('/', methods=["GET","POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        print("entrou")
+        target = str(request.form.get('target'))
+        dependent = get_dependents(grafo, target)
+        dependencie = get_dependencies(grafo, target)
+        return render_template('list.html', target=target, dependent=dependent, dependencies=dependencie)
+        # return f"<h1>{target}</h1>"
+
 
 @app.route('/dependencies/<string:target>')
 def dependencies(target):
