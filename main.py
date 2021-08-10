7cf52e85581e1b573a174056c369c0e93d7766ef
Matheus Gabrie <matheuscoding@gmail.com>
2021-08-09 23:04:22 -0300

    dataset bug fix
    Co-authored-by: pedrok99 <pedrohlima20@gmail.com>

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ df = read_csv('grade_software.csv').values.tolist()
 
 edges = []
 
+# print(df)
 for lista in df:
   lista[1] = eval(lista[1])
+  if len(lista[1]) == 0:
+    edges.append((lista[0], ''))
   for dependencie in lista[1]:
+    # print(f"{lista[0]} -> {dependencie}")
     edges.append((lista[0], dependencie))
+# print(edges)
 
 grafo = Graph(edges = edges, directed = True)
+
 
 
 @app.route('/', methods=["GET","POST"])
 def index():
     if request.method == "GET":
         vertices = grafo.get_vertices()
+        vertices.remove('')
         return render_template("index.html",vertices=vertices)
     else:
         target = str(request.form.get('target')).replace("_"," ")
         dependent = get_dependents(grafo, target)
+        # dependent[0] = [i for i in dependent[0] if i != '']
+        dependent[0] = ['Nenhuma matéria' for i in dependent[0] if i == '']
         dependencie = get_dependencies(grafo, target)
         return render_template('list.html', target=target, dependent=dependent, dependencies=dependencie)
         # return f"<h1>{target}</h1>"
